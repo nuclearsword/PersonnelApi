49cd696e76dc3abb17dbc3c43ecdbf74f7984bcf
Kyle Dunbar <kyle.dunbar@eventcore.com>
2019-05-20 10:48:12 -0700

    Rename controller. Add get method to retrieve all personnel.

---
+++ Controllers/PersonnelController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PersonnelApi.Models;
+using Newtonsoft.Json;
+using Microsoft.EntityFrameworkCore;
+
+namespace PersonnelApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonnelController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Person>>> Get(){
+            using(var ctx = new KDTestContext()){
+                return await ctx.Personnel.ToListAsync();
+            }
+        }
+    }
+}
